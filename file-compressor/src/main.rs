dd97f707466c27640b21aace9e198d9b111b784e
Logan <ninetailsoftware@gmail.com>
2024-04-04 22:02:00 -0400

    Gcd is poop and wirtten wrong. Will have to come back to it.

--- file-compressor/src/main.rs
+++ file-compressor/src/main.rs
@@ -11,13 +11,37 @@ fn read_file_to_string(path: &str) {
 }
 
 fn read_file_to_bytes(path: &str) {
+    let mut odd_gcd = 0;
+    let mut highest_odd_gcd = 0;
+    let mut even_gcd = 0;
+    let mut highest_even_gcd = 0;
     if let Ok(f) = File::open(path) {
         let mut reader = BufReader::new(f);
         let mut buffer = Vec::new();
 
         if let Ok(_) = reader.read_to_end(&mut buffer) {
             for byte in &buffer {
-                println!("BYTE: {}", byte);
+                for number in 2..127 {
+                    if byte % 2 == 0 {
+                        if byte / number == 0 {
+                            let even_gcd: u8 = *(byte);
+                            if even_gcd % number != 0 {
+                                highest_even_gcd = number;
+                                // println!("BYTE: {} | Even {}", byte, number);
+                            }
+                        }
+                    }
+                    if byte % 2 != 0 {
+                        if byte / number != 0 {
+                            let odd_gcd: u8 = *(byte);
+                            if odd_gcd % number == 0 {
+                                highest_odd_gcd = number;
+                                // println!("BYTE: {} | Odd {}", byte, number);
+                            }
+                        }
+                    }
+                }
+                // println!("BYTE: {}", byte);
             }
         } else {
             eprintln!("Error reading file: {}", path);
@@ -25,6 +49,7 @@ fn read_file_to_bytes(path: &str) {
     } else {
         eprintln!("Error opening file: {}", path);
     }
+    println!("Highest Odd GCD {} | Highest Even GCD {}", highest_odd_gcd, highest_even_gcd)
 }
 
 fn read_file_to_bits(path: &str) {
@@ -56,3 +81,6 @@ fn main() {
     read_file_to_bytes(path);   
     // read_file_to_bits(path);  ehh
 }
+
+
+// Compress if just even maybe and can figure it out based on if number is even when recieved because if it is odd it would stay that way.
